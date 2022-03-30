44fb3dfb12daa9d1e009864200efd07cc8565d88
john <jallen@conversantmedia.com>
2022-03-29 19:38:36 -0500

    page components & html layout

---
+++ src/app/shared/components/page-skills/page-skills.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-skills',
+  templateUrl: './page-skills.component.html',
+  styleUrls: ['./page-skills.component.scss']
+})
+export class PageSkillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,7 +6,10 @@ import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
 import { HeaderComponent } from './components/header/header.component';
 import { PageAboutComponent } from './components/page-about/page-about.component';
+import { PageContactComponent } from './components/page-contact/page-contact.component';
 import { PageHomeComponent } from './components/page-home/page-home.component';
+import { PagePortfolioComponent } from './components/page-portfolio/page-portfolio.component';
+import { PageSkillsComponent } from './components/page-skills/page-skills.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 
@@ -17,6 +20,9 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
         HeaderComponent,
         PageHomeComponent,
         PageAboutComponent,
+        PageSkillsComponent,
+        PagePortfolioComponent,
+        PageContactComponent,
     ],
     imports: [
         CommonModule,
@@ -30,7 +36,10 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
         SidenavComponent,
         HeaderComponent,
         PageHomeComponent,
-        PageAboutComponent
+        PageAboutComponent,
+        PageSkillsComponent,
+        PagePortfolioComponent,
+        PageContactComponent,
     ],
 })
 export class SharedModule { }
